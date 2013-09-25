5e1d504990e97f515ff872694437e4858983b412
Noumbissi <xnoumbis@uwaterloo.ca>
2013-09-25 18:39:21 -0400

    Some new changes
    	renamed:    webapps/ROOT/WEB-INF/ta-sink.txt -> webapps/ROOT/WEB-INF/ta-sink.cfg
    	modified:   webapps/ROOT/WEB-INF/web.xml
    	modified:   webapps/ROOT/test/index.php
    	modified:   webapps/ROOT/test/test-sql.php

--- webapps/ROOT/test/index.php
+++ webapps/ROOT/test/index.php
@@ -1,7 +1,5 @@
-<!--<form action="welcome.php?sample=1" method="post">-->
 <form action="test-sql.php?table=desk" method="POST">
 Name: <input type="text" name="fname"><br/>
 Age: <input type="text" name="age"><br/>
 <input type="submit">
 </form>
-<!--<?php phpinfo(); ?>-->
--- webapps/ROOT/test/test-sql.php
+++ webapps/ROOT/test/test-sql.php
@@ -1,29 +1,25 @@
 <?php
 // Create connection
- $con=mysqli_connect("localhost","pepsi","cola","test");
+$con=mysqli_connect("localhost","pepsi","cola","test");
 
 $desk = $_GET["table"];
 
-echo "Here is the name: " . $_POST["fname"] . '\n'
-
-#print "Table is: " . $desk . '\n';
+echo "Entered Name: " . $_POST["fname"] . "\n"
+echo "Entered Age: " . $_POST["age"] . "\n"
 
 // Check connection
-if (mysqli_connect_errno())
-{
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error() . '\n';
   exit();
 }
-else  
-{
+else {
   $sql = "SELECT * FROM " . $desk . ";";
-  
-  print "Query to execute: " . $sql . "\n";
-
+  #print "Query to execute: " . $sql . "\n";
   $result = mysqli_query($con, $sql);
 
   if ($result) {
     printf("Select returned %d rows.\n", mysqli_num_rows($result));
+    printf("Query was %s .\n", $sql);
     mysqli_free_result($result);
   }
   else {
@@ -31,12 +27,9 @@ else
     echo 'MySQL Error: ' . mysqli_error($con) . "\n";
     exit;
   }
-  
   mysqli_free_result($result);
 }
 
 echo "Now closing connection\n";
-
 mysqli_close($con);
-
 ?> 
