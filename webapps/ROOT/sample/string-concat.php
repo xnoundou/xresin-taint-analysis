eeac6137cb79294f14c440dafcf9a19b55ab4d45
Noumbissi <xnoumbis@uwaterloo.ca>
2013-10-08 00:56:44 -0400

    In progress: added new test cases
    	new file:   string-concat.expect
    	new file:   string-concat.php
    	modified:   substraction.expect
    	modified:   substraction.php

---
+++ webapps/ROOT/sample/string-concat.php
@@ -0,0 +1,5 @@
+<?php
+$req1 = $_GET["req1"];
+$result = 'a string from GET ' . $req1;
+printf ("Result: %s\n", $result);
+?>
--- webapps/ROOT/sample/substraction.php
+++ webapps/ROOT/sample/substraction.php
@@ -1,5 +1,4 @@
 <?php
-
 $req1 = $_GET["req1"];
 $result = 11 - $req1;
 printf ("Result: %f\n", $result);
